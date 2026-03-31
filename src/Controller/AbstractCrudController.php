e5d0f026d25a2c19c655186c01a10f2fb25123cf
dev <dev@dev.com>
2026-03-31 19:06:36 +0300

    [fix] Cast entity id to string in navbar to fix active state on update

--- src/Controller/AbstractCrudController.php
+++ src/Controller/AbstractCrudController.php
@@ -721,7 +721,7 @@ abstract class AbstractCrudController extends AbstractController implements Crud
                             'label' => $this->getEntityLabel($parent, $parentOptions),
                         ], 'cms'),
                         'route' => $parentUpdate['route'],
-                        'route_params' => $parentUpdateRouteParams + ['id' => $parent->getId()], // @phpstan-ignore method.notFound
+                        'route_params' => $parentUpdateRouteParams + ['id' => (string) $parent->getId()], // @phpstan-ignore method.notFound
                     ]);
                 }
             }
@@ -732,7 +732,7 @@ abstract class AbstractCrudController extends AbstractController implements Crud
                 /** @var array<string, mixed> $params */
                 $params = $prefixOptions['route_params'];
                 if (null !== $entity) {
-                    $params['id'] = $entity->getId(); // @phpstan-ignore method.notFound
+                    $params['id'] = (string) $entity->getId(); // @phpstan-ignore method.notFound
                 }
 
                 /** @var string $transId */
